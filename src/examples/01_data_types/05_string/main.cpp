638ddcf82975418a30c1298a1c0ea5f7327c7b42
ChristopherRomero00 <christopher.romero2@g.austincc.edu>
2022-06-04 17:54:32 -0500

    Updated files

--- src/examples/01_data_types/05_string/main.cpp
+++ src/examples/01_data_types/05_string/main.cpp
@@ -1,7 +1,13 @@
+#include<iostream>
+#include<string>
 
+using std::cout;
+using std::string;
 //create string variable, initialize and display
 int main()
 {
+    string name = "C++ is 11";
+    cout<<"Language is "<<name<<"\n";
 
     return 0;
 }
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,9 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "multi_assign.h"
+#include "expressions.h"
+#include "casting.h"
+#include "constants.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -10,4 +13,29 @@ TEST_CASE("Test function multiply numbers")
 {
 	REQUIRE(multiply_numbers(10) == 50);
 	REQUIRE(multiply_numbers(2) == 10);
+}
+
+TEST_CASE("Test operator precedence no parentesis params: 12, 6, 3")
+{
+	REQUIRE(operator_precedence_1(12, 6, 3) == 14);
+}
+
+TEST_CASE("Test operator precedence with parentesis params: 12, 6, 3")
+{
+	REQUIRE(operator_precedence_2(12, 6, 3) == 6);
+}
+
+TEST_CASE("Test that convert to double function returns a double(decimal)")
+{
+	REQUIRE(convert_to_double(10, .51) == 5.1);
+}
+
+TEST_CASE("Test function convert double to int to verify that it's a whole number")
+{
+	REQUIRE(convert_to_double(5.1) == 5);
+}
+
+TEST_CASE("Test get area function")
+{
+	REQUIRE(get_area_of_circle(10) == 314.159);
 }
