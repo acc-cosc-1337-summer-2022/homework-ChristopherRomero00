a5b3c36b5efbfb7bbd7120d56bdb6fd9f2fe6e42
ChristopherRomero00 <christopher.romero2@g.austincc.edu>
2022-06-18 16:10:34 -0500

    HW 3

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,13 @@
 //write include required statements
+#include<iostream>
+#include<string>
+
+using std::string;
 
 
 //Write a function(s) prototype  here
-
+int get_grade_points(char letter_grade);
+double calculate_gpa(int creditHours, double credit_points);
 
 
 
