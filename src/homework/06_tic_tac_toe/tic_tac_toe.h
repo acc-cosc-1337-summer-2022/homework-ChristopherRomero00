9b6f6dbc9eac901f4e6b118d3a9f0179ced64b38
ChristopherRomero00 <christopher.romero2@g.austincc.edu>
2022-07-16 17:56:16 -0500

    TicTacToe HW

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -2,30 +2,36 @@
 #include<iostream>
 #include<string>
 #include<vector>
+
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
+
+using std::cout;
 
 class TicTacToe
 {
 public:
+    TicTacToe(int size) : pegs(size*size, " " ){};
     bool game_over();
     void start_game(std::string first_player);
     void mark_board(int position);
     std::string get_player() const{return player;}
-    void display_board() const;
+    virtual void display_board() = 0;
     std::string get_winner() {return winner;}
     
 private:
     std::string player;
-    std::vector<std::string> pegs{9, " "}; 
     std::string winner;
     void clear_board();
     void set_next_player();
     bool check_board_full();
-    bool check_column_win();
-    bool check_row_win();
-    bool check_diagonal_win();
     void set_winner();
+
+protected:
+    std::vector<std::string> pegs;
+    virtual bool check_column_win() = 0; //pure virtual functions = 0
+    virtual bool check_row_win() = 0;
+    virtual bool check_diagonal_win() = 0;
 };
 
 #endif
--- src/homework/06_tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,13 @@
-//h
+//h 
+#include "tic_tac_toe.h"
+
+class TicTacToe4 : public TicTacToe
+{
+public:
+    TicTacToe4() : TicTacToe(4){}
+private: 
+    bool check_column_win()override;
+    bool check_row_win()override;
+    bool check_diagonal_win() override;
+    void display_board()override;
+};
